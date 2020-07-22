d010ce5d88d723c1158a34e9d5e6f555de0bfd39
Sahand Babali <42813816+sahandbabali@users.noreply.github.com>
2020-07-22 17:38:36 +0430

    Update TRexRunnerMode.js

--- TRexRunnerMode.js
+++ TRexRunnerMode.js
@@ -8,6 +8,14 @@ function godmode() {
 }
 
 
+function skinchager() {
+      let select = document.getElementById("skins");
+      let value = select.options[select.selectedIndex].value;
+      console.log(value);
+
+}
+
+
 function setspeed(speed) {
       let newspeed = speed;
       Runner.instance_.setSpeed(newspeed);
@@ -21,21 +29,27 @@ function resethacks() {
 }
 
 
-
-
-godmode();
-setspeed(100);
-
-// resethacks();
-
-
 // UI
-
 var div = document.getElementById('main-content');
 var newdiv = document.createElement( 'div' ); 
-newdiv.style.backgroundColor = "black";
+newdiv.style.backgroundColor = "#535353";
 newdiv.style.color = "white";
-newdiv.innerHTML = '<p>Test</p>';
+newdiv.style.padding = "1em";
+
+newdiv.innerHTML = '<form action="#" method="get"> <p>Menu</p> <span>God Mode</span> <span style="border: 1px solid white; padding: 2px" id="godmode">Activate</span> </br> <span>Speed</span> <input type="text"></input> </br> <span>Skin</span> <select name="skins" id="skins"> <option value="Color">Color</option> <option value="Salad Cat">Salad Cat</option> </select> </form>';
 div.parentNode.insertBefore( newdiv, div.nextSibling );
+
+// Remove main message
+document.getElementById('main-message').remove();
+
+
+
+// Events
+
+document.getElementById("godmode").addEventListener("click", godmode );
+
+document.getElementById("skins").addEventListener("change", skinchager);
+
+
 
 
